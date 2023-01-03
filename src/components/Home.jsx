902c130f7e9172e420f2867ea44045a47af44199
jacobcurry <jacob.a.curry@gmail.com>
2023-01-03 09:01:17 -0500

    basic website

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Logo from "../assets/Logo.png";
+import { Navigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <div className="home-post-container">
+        <img className="post-logo" src={Logo} alt="Logo" />
+        <div
+          onClick={() => (window.location.pathname = "/post")}
+          className="fake-input"
+        >
+          Create Post
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
